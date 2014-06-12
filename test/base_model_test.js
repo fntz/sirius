652930f0c84962018a23ebf7381602b8eacb16da
mike <fantazuor@gmail.com>
2014-06-12 21:57:11 +0300

    to_json test

--- test/base_model_test.js
+++ test/base_model_test.js
@@ -66,7 +66,7 @@ suite("BaseModel", function() {
     assert(j0 == result);
 
     var j1 = m.to_json(true);
-    var result = JSON.stringify({"my-model": JSON.parse(result)});
+    var result = JSON.stringify({"my_model": JSON.parse(result)});
     assert(j1 == result);
   });
 
@@ -74,6 +74,7 @@ suite("BaseModel", function() {
     SiriusApplication.adapter = new JQueryAdapter();
     var m = new MyModel({"id": 10, "title": "my title", "description": "text..."});
     var r = m.to_html();
+
     assert($(r[0]).prop('tagName') == "B");
     assert($(r[0]).text() == "10");
     assert($(r[0]).attr('class') == 'my-model-id');
@@ -99,11 +100,60 @@ suite("BaseModel", function() {
 
   test("#from_html", function() {
     SiriusApplication.adapter = new JQueryAdapter();
-    var m = MyModel.from_html("#my-model-form");
+    var m = MyModel.from_html("#my_model_form");
 
     assert(m.get("id") == 1);
     assert(m.get("title") == "new title");
     assert(m.get("description") == "text...");
+  });
+
+  suite("relations", function() {
+    var p0 = new Person({id: 1}),
+        p1 = new Person({id: 2}),
+        n0 = new Name({name: "abc"}),
+        n1 = new Name({name: "qwe"}),
+        g0 = new Group({name: "group-0"}),
+        g1 = new Group({name: "group-1"}),
+        g2 = new Group({name: "group-2"});
+
+        p0.add_group(g0);
+        p0.add_group(g1);
+        p1.add_group(g2);
+        p0.add_name(n0);
+
+    test("#has_*", function() {
+      assert(p0.attributes.length == 3);
+      assert(p0.get("group").length == 2);
+      assert(p1.get("group").length == 1);
+
+      //check feedback
+      assert(g0.attributes.length == 2);
+
+      assert(g0.get('person_id') == 1);
+      assert(g1.get('person_id') == 1);
+      assert(g2.get('person_id') == 2);
+
+      assert.throw(function() { p0.add_name(n1);});
+    });
+
+    test("#to_json&html", function() {
+      SiriusApplication.adapter = new JQueryAdapter();
+
+      var c = function(m){console.log(m);}
+      var z = {"id":1,"group":[{"name":"group-0","person_id":1},{"name":"group-1","person_id":1}],"name":{"name":"abc","person_id":1}};
+
+      var j = JSON.parse(p0.to_json());
+
+      assert(z["id"] == j["id"]);
+      assert(z["name"]["person_id"] == j["name"]["person_id"]);
+      assert(z["name"]["name"] == j["name"]["name"]);
+      assert(j["group"].length == 2);
+      assert(z["group"][0]["name"] == j["group"][0]["name"]);
+
+
+      //$("body").append(h)
+    });
+    test("#from_json, from_html");
   });
 
 
